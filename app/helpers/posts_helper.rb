fc90c7294778fdda32b2efb6c3742c30135ead76
Alex Daish <alexdaish@Alexs-MacBook-Pro.local>
2015-04-29 18:02:57 +0100

    added bug fix for publication

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,16 @@
 module PostsHelper
+	def pull(quote)
+		if quote.present?
+			"<blockqoute>#{quote}</blockqoute>".html_safe
+			end
+	end
+
+	def link_to_publication(post, opts = {})
+		if opts [:seperator]
+		"<span class=\"separator\">•</span>Published in #{link_to post.publication, post.url} on #{post.published_at}".html_safe
+	else	
+		"Published in #{link_to post.publication, post.url} on #{post.published_at}".html_safe
+	end
+end
+
 end
